6a18b681abc1edf7e44b46de4687127b406e2ee0
Kento75 <kento2github@gmail.com>
2019-01-20 09:14:05 +0900

    Add spring-security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,7 @@ VALUES(1, '山田太郎', 30);
 /* ユーザマスタのデータ（管理者権限） */
 INSERT INTO m_user(user_id, password, user_name, birthday, age, marriage, role)
 VALUES('kento75@xxx.co.jp', 'password', 'タカノケント', '1993-07-05', 25, false, 'ROLE_ADMIN');
+
+/* ユーザマスタのデータ（一般権限） */
+INSERT INTO m_user(user_id, password, user_name, birthday, age, marriage, role)
+VALUES('test@xxx.co.jp', 'password', 'テストタロウ', '1995-04-02', 23, false, 'ROLE_GENERAL');
