2d03d663d04ed8173dc7aca581b52dc48e305d5f
Kento75 <kento2github@gmail.com>
2019-01-20 12:15:16 +0900

    Add Auth logout logic

--- src/main/kotlin/com/example/demo/SecurityConfig.kt
+++ src/main/kotlin/com/example/demo/SecurityConfig.kt
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 import javax.sql.DataSource
 
 
@@ -68,6 +69,12 @@ class SecurityConfig: WebSecurityConfigurerAdapter() {
                 .usernameParameter("userId")       // ログイン画面のユーザーID
                 .passwordParameter("password")     // ログイン画面のパスワード
                 .defaultSuccessUrl("/home", true)   // ログイン成功後の遷移先
+
+        // ログアウト処理
+        http.logout()
+                .logoutRequestMatcher(AntPathRequestMatcher("/logout"))
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
 
         // CSRF対策を無効に設定（後で消す）
         http.csrf().disable()
